f41c198942ed83c2337073ba9f8b2e16bb06e6ad
djay43 <44063126+djay43@users.noreply.github.com>
2018-12-01 17:38:28 +0100

    Init project - index structure

---
+++ index_view.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Jean forteroche blog</title>
+   	    <link href="./public/css/style.css" rel="stylesheet" />
+
+    </head>
+
+    <header>
+        
+        <h1>Billet simple pour l'Alaska</h1>
+        <a href="./index.php">Accueil</a>
+        <a href="./index.php?action=authentification">S'identifier</a>
+
+        
+    </header>
+    
+    <body>
+    
+       <section>
+           <article id="main_content">
+                <h3>Dernière publication:</h3>
+               
+                      
+                       
+           </article>      
+        </section>
+      
+           
+           <div id="last_episodes_content">
+                <article class="last_post">
+                    <div id="last_less1">
+                            
+                    </div>
+               
+                </article>
+                <article class="last_post">
+                    <div id="last_less2">
+                            
+
+                    </div>
+               </article>
+               <article class="last_post">
+                    <div id="last_less3">
+                            
+
+                    </div>
+                </article>
+            </div>
+</body>
+    
+<footer>
+    
+</footer>
+</html>
